65d51263843100a8a3ab0ccb52cef26426420179
Mike Santoro <mike.santoro42@gmail.com>
2019-05-28 19:08:22 -0400

    basic ruby math class and methods

--- test.rb
+++ test.rb
@@ -4,3 +4,17 @@ print "Hello World"
 print "same line"
 puts "same line then different line"
 puts "gains"
+phrase = "gains are the best"
+puts phrase.upcase()
+puts phrase[1].upcase()
+puts phrase[0,5].downcase()
+puts phrase.index("g")
+puts "string".upcase()
+
+# working with numbers
+puts -5.3
+puts 2 + 2
+puts 2**3
+num = 4
+puts ("this is a num and a string together " + num.to_s)
+puts Math.sqrt(num)
