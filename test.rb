35f505384bb16884200d829d7a511e252f7aabf6
Mike Santoro <mike.santoro42@gmail.com>
2019-05-29 18:01:09 -0400

    ruby tutorial scratch (methods)

--- test.rb
+++ test.rb
@@ -1,20 +1,70 @@
 
 
-print "Hello World"
-print "same line"
-puts "same line then different line"
-puts "gains"
-phrase = "gains are the best"
-puts phrase.upcase()
-puts phrase[1].upcase()
-puts phrase[0,5].downcase()
-puts phrase.index("g")
-puts "string".upcase()
+# Methods in Ruby
 
-# working with numbers
-puts -5.3
-puts 2 + 2
-puts 2**3
-num = 4
-puts ("this is a num and a string together " + num.to_s)
-puts Math.sqrt(num)
+def sayhi
+  puts "hello"
+end
+
+sayhi
+
+# # Hashes (key value pair)
+# states = {
+#   "pennsylvania" => "PA",
+#   "maryland" => "MA"
+# }
+#
+# puts states
+# puts states["maryland"]
+
+
+# # Arrays
+#
+#   stuff = Array[1, 2, 3, 4]
+#   puts stuff
+#
+#   stuff2 = Array.new
+#   stuff2[2] = 1
+#   puts stuff2
+#   puts stuff2.include? 1
+
+# building a calculator in Ruby
+# puts "Enter a number: "
+# num1 = gets.chomp()
+# puts "Enter another number: "
+# num2 = gets.chomp()
+#
+# puts (num1.to_i + num2.to_i)
+
+# to_i integer
+# to_f floating points
+
+
+
+
+# # user input
+# puts "Enter your name: "
+# name = gets.chomp()
+# puts ("Hello " + name + " chomp eliminates a new line")
+
+
+
+
+# print "Hello World"
+# print "same line"
+# puts "same line then different line"
+# puts "gains"
+# phrase = "gains are the best"
+# puts phrase.upcase()
+# puts phrase[1].upcase()
+# puts phrase[0,5].downcase()
+# puts phrase.index("g")
+# puts "string".upcase()
+#
+# # working with numbers
+# puts -5.3
+# puts 2 + 2
+# puts 2**3
+# num = 4
+# puts ("this is a num and a string together " + num.to_s)
+# puts Math.sqrt(num)
