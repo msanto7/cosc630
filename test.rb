666b9ec30f32efadf97967ed4a8f6ae5050d9e9c
Mike Santoro <mike.santoro42@gmail.com>
2019-05-31 18:06:31 -0400

    scratch for up to for loops

--- test.rb
+++ test.rb
@@ -1,7 +1,62 @@
 
+
+
+
+
+
+
+
+
+
+
+# # FOR LOOPS
+# friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+#
+# for friend in friends
+#   puts friend
+# end
+#
+# friends.each do |friend|
+#   puts friend
+# end
+#
+# for index in 0..5
+#   puts index
+# end
+#
+# 6.times do |index|
+#   puts index
+# end
+
+
 # WHILE LOOPS
 
+# secret_word = "giraffe"
+# guess = ""
+# guess_count = 0
+# guess_limit = 3
+# out_of_guesses = false
+#
+# while guess != secret_word and !out_of_guesses
+#   if guess_count < guess_limit
+#     puts "Enter your guess: "
+#     guess = gets.chomp()
+#     guess_count += 1
+#   else
+#     out_of_guesses = true
+#   end
+# end
+# if out_of_guesses
+#   puts "You Lost!"
+# else
+#   puts "You Won!"
+# end
 
+  # index = 1
+  # while index <= 5
+  #   puts index
+  #   index += 1
+  # end
 
 
 # CASE STATEMENTs
